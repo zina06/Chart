9fcb0417e7077af7a3f5490f66c182e3b0cedc50
zina06 <dhaud42@naver.com>
2024-05-13 14:36:07 +0900

    first commit

---
+++ src/common/common.js
@@ -0,0 +1,48 @@
+import Chart from 'chart.js/auto';
+
+export const drawChart = (ctx, time, ping) => {
+    
+   console.log("drawChart");
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: time.value,
+        datasets: [{
+          label: 'ping',
+          data: ping.value,
+          borderColor: 'rgb(75, 192, 192)',
+          backgroundColor: 'rgb(75, 192, 192)',
+          borderWidth: 1,
+          fill: false,
+          pointRadius : 0
+        }]
+      },
+      options: {    
+        plugins: {
+            legend: {
+                position: 'bottom', // 범례 위치
+                align: 'start' // 범례 수직 정렬
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            type: 'time',
+            time: {
+              unit: 'day',
+              displayFormats: {
+                day: 'MM/dd'
+              }
+            }
+          },
+          y: {
+            min: 0,
+            max: 60000
+          }
+        }
+      }
+    });
+  };
+
+
